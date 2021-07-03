960fcc5c33b729a600c2fbf1b55da534ff9d6a1b
Nevaland <deanh0716@gmail.com>
2021-07-03 18:14:14 +0900

    Refactoring urls

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,7 +1,12 @@
-from django.urls import path
+from django.urls import path, include
 from accounts import views
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+router.register(r'users', views.UserViewSet)
 
 urlpatterns = [
+    path('', include(router.urls)),
     path('google/login', views.google_login, name='google_login'),
     path('google/callback/', views.google_callback, name='google_callback'),
     path('google/login/finish/', views.GoogleLogin.as_view(), name='google_login_todjango'),
--- config/urls.py
+++ config/urls.py
@@ -1,13 +1,16 @@
 from django.urls import path, include
 from django.contrib import admin
+from django.views.generic import RedirectView
 
 urlpatterns = [
-    path('', include('board.urls')),
-    path('user/', include('accounts.urls')),
+    path('', RedirectView.as_view(url='/boards')),
+    path('boards/', include('board.urls')),
+    path('accounts/', include('accounts.urls')),
+
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     path('admin/', admin.site.urls),
+
     path('accounts/', include('dj_rest_auth.urls')),
     path('accounts/', include('dj_rest_auth.registration.urls')),
     path('accounts/', include('allauth.urls')),
-    path('accounts/', include('accounts.urls')),
 ]
