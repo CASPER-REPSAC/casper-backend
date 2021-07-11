2a9d6c0ba31927fcf85903b3334942b6ebf31926
Nevaland <deanh0716@gmail.com>
2021-07-11 23:27:27 +0900

    Divide settings for production

--- config/settings/base.py
+++ config/settings/base.py
@@ -17,7 +17,7 @@ import sys
 from datetime import timedelta
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 ROOT_DIR = os.path.dirname(BASE_DIR)
 SECRET_BASE_FILE = os.path.join(BASE_DIR, 'secrets.json')
@@ -29,11 +29,7 @@ for key, value in secrets.items():
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
 ALLOWED_HOSTS = ['*']
-
 
 # Application definition
 
