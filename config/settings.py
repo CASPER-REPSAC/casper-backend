b2aaa6fe6cb57ff772f4f766c7bea1ca6dd5ac2d
Nevaland <deanh0716@gmail.com>
2021-07-11 21:34:49 +0900

    Remove secret key

--- config/settings.py
+++ config/settings.py
@@ -29,9 +29,6 @@ for key, value in secrets.items():
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-+@a^5o!il)5fcxnw(w!3o9laont7+bh%3l91-*x158d2)_csu8'
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
